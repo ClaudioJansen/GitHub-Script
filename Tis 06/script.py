65fce003fbfd6bcd15c121ead48173cb5389c8a2
ClaudioJansen <caujansen@gmail.com>
2023-10-02 23:47:54 -0300

    Buscar motivo de falha mais tempo de correçao da pipeline

--- Tis 06/script.py
+++ Tis 06/script.py
@@ -1,7 +1,9 @@
 import requests
+import csv
+from datetime import datetime
 
 # Adicionar token
-token = 'seu_token'
+token = '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'
 
 params = {
     'q': 'language:java',
@@ -11,45 +13,68 @@ params = {
 }
 
 url = 'https://api.github.com/search/repositories'
+headers = {'Authorization': f'token {token}'}
 
 pipelines_info = []
 
 def get_pipeline_info(owner, repo):
     pipeline_url = f'https://api.github.com/repos/{owner}/{repo}/actions/workflows'
-    headers = {'Authorization': f'token {token}'}
     response = requests.get(pipeline_url, headers=headers)
     if response.status_code == 200:
-        try:
-            data = response.json()
-            return data
-        except ValueError:
-            pass 
+        return response.json()
+    return None
+
+def get_failed_runs(owner, repo, workflow_id):
+    runs_url = f'https://api.github.com/repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs'
+    response = requests.get(runs_url, headers=headers)
+    if response.status_code == 200:
+        runs_data = response.json()
+        return [run for run in runs_data['workflow_runs'] if run['conclusion'] == 'failure']
+    return []
+
+def get_time_to_fix(owner, repo, workflow_id, failed_run_id):
+    runs_url = f'https://api.github.com/repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs'
+    response = requests.get(runs_url, headers=headers)
+    if response.status_code == 200:
+        runs_data = response.json()
+        found_error = False
+        error_time = None
+        for run in runs_data['workflow_runs']:
+            if run['id'] == failed_run_id:
+                found_error = True
+                error_time = run['created_at']
+            elif found_error and run['conclusion'] == 'success':
+                success_time = run['created_at']
+                FMT = '%Y-%m-%dT%H:%M:%SZ'
+                tdelta = datetime.strptime(success_time, FMT) - datetime.strptime(error_time, FMT)
+                return str(tdelta)
     return None
 
 for page in range(1, 2):
     params['page'] = page
-    response = requests.get(url, params=params)
+    response = requests.get(url, headers=headers, params=params)
     if response.status_code == 200:
-        try:
-            data = response.json()
-            if 'items' in data and isinstance(data['items'], list):
-                for repo in data['items']:
-                    owner = repo['owner']['login']
-                    repo_name = repo['name']
-                    pipelines = get_pipeline_info(owner, repo_name)
-                    if pipelines:
-                        pipelines_info.append({'repo': repo_name, 'owner': owner, 'pipelines': pipelines})
-        except ValueError:
-            pass 
+        data = response.json()
+        for repo in data['items']:
+            owner = repo['owner']['login']
+            repo_name = repo['name']
+            pipelines = get_pipeline_info(owner, repo_name)
+            if pipelines:
+                pipelines_info.append({'repo': repo_name, 'owner': owner, 'pipelines': pipelines})
 
-for repo_info in pipelines_info:
-    print(f"Repositório: {repo_info['owner']}/{repo_info['repo']}")
-    print(f"Total de pipelines: {repo_info['pipelines']['total_count']}")
-    for workflow in repo_info['pipelines']['workflows']:
-        print(f"Nome da Pipeline: {workflow['name']}")
-        print(f"ID da Workflow: {workflow['id']}")
-        print(f"Estado: {workflow['state']}")
-        print(f"Link para a Pipeline: {workflow['html_url']}")
-        print('-' * 40)
-        
-       
+with open('pipelines.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(['repository_name', 'pipeline_name', 'logs_url', 'fix_time'])
+
+    for repo_info in pipelines_info:
+        for workflow in repo_info['pipelines']['workflows']:
+            failed_runs = get_failed_runs(repo_info['owner'], repo_info['repo'], workflow['id'])
+            for run in failed_runs:
+                fix_time = get_time_to_fix(repo_info['owner'], repo_info['repo'], workflow['id'], run['id'])
+                if fix_time:
+                    writer.writerow([
+                        f"{repo_info['owner']}/{repo_info['repo']}",
+                        workflow['name'],
+                        run['logs_url'],
+                        fix_time
+                    ])
